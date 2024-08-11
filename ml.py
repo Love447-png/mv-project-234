40ba38f978e54ac0c20a9f4f9b9e3dc3e1035683
*Love447-png <*varunkhandelwal16nov@gmail.com>
2024-08-11 11:43:19 +0530

    Add a array

--- ml.py
+++ ml.py
@@ -1,7 +1,10 @@
-from sklearn import linear_model
-regr = linear_model.LinearRegression()
-x = np.asanyarray(train[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
-y = np.asanyarray(train[['CO2EMISSIONS']])
-regr.fit (x, y)
-# The coefficients
-print ('Coefficients: ', regr.coef_)
+arr = [10,20,30,40]
+def LShift(arr,n):
+    L=len(arr)
+    for x in range(0,n):
+        y = arr[0]
+        for i in range(0,L-1):
+           arr[i]=arr[i+1]
+        arr[L-1] = y
+    print(arr)
+LShift(arr,2)    
